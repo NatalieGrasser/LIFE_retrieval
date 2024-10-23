d6a21436092ced54f82180460adaa0a9a55c78a0
NatalieGrasser <grasser@strw.leidenuniv.nl>
2024-10-23 12:27:09 +0200

    PT profile

--- LIFE_retrieval/figures.py
+++ LIFE_retrieval/figures.py
@@ -110,7 +110,7 @@ def plot_pt(retrieval_object,fs=12,**kwargs):
         return xmin,xmax
 
     xmin,xmax=plot_temperature(retrieval_object,ax,olabel='Retrieval')
-    model_object=pRT_spectrum(retrieval_object)
+    model_object=pRT_spectrum(retrieval_object,contribution=True)
     summed_contr=model_object.contr_em
     contribution_plot=summed_contr/np.max(summed_contr)*(xmax-xmin)+xmin
     ax.plot(contribution_plot,retrieval_object.model_object.pressure,linestyle='dashed',
