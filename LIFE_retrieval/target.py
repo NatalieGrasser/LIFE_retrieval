cb7724161e6c49a31fdd7e326e58e3993b010ac9
NatalieGrasser <grasser@strw.leidenuniv.nl>
2024-11-13 13:33:39 +0100

    updated data error

--- LIFE_retrieval/target.py
+++ LIFE_retrieval/target.py
@@ -26,15 +26,16 @@ class Target:
             file1=np.genfromtxt(filename,skip_header=13,skip_footer=1488,delimiter='  ')
             wl1=file1[:,0]
             fl1=file1[:,1]
-            flerr1=np.ones_like(fl1)*1e-5
+            #flerr1=np.ones_like(fl1)*1e-5 
             file2=np.genfromtxt(filename,skip_header=424,delimiter='  ')
             wl2=file2[:,0]
             fl2=file2[:,1]
-            flerr2=file2[:,2]
+            #flerr2=file2[:,2] # why so high at the beginning?
             self.wl=np.append(wl1,wl2)
             self.fl=np.append(fl1,fl2)
             self.fl/=np.median(self.fl)
-            self.flerr=np.append(flerr1,flerr2)
+            #self.flerr=np.append(flerr1,flerr2) 
+            self.flerr = np.ones_like(self.fl)*1e-5
         return self.wl,self.fl,self.flerr
         
     def get_mask_isfinite(self):
